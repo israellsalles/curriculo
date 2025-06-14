c705559edefbb3e65c9df9cce5212b8028995642
israellsalles <israelsales@outlook.com>
2025-06-13 23:08:16 -0300

    Atualizacao

---
+++ script.js
@@ -0,0 +1,5 @@
+// Interatividade simples
+
+document.getElementById('surpriseButton').addEventListener('click', function() {
+    alert('Gratidão por visitar meu currículo! Vamos juntos construir um mundo melhor com tecnologia e consciência 🌟');
+});
